7d9d7a8889fccacab35401e4ea7a04544a23bb4b
Sahara138 <saharaiislam586@gmail.com>
2026-03-05 15:59:35 +0600

    Updated Vendor

--- src/pages/brokerDashboard/UpdateVendorProperty/UpdateVendorProperty.tsx
+++ src/pages/brokerDashboard/UpdateVendorProperty/UpdateVendorProperty.tsx
@@ -921,7 +921,7 @@ const UpdateVendorProperty: React.FC = () => {
                         {/* Image Upload Section */}
                         <div className="mb-6 border border-gray-200 rounded-lg p-4">
                             <h2 className="text-2xl font-semibold text-gray-900 mb-1">
-                                Please upload images (size less than 100KB)
+                                Please upload images (size less than 10MB)
                             </h2>
 
                             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
