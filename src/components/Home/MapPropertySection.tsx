916837f5caee40262e1987690ede92144e0a1fc3
Sahara138 <saharaiislam586@gmail.com>
2026-03-09 12:58:10 +0600

    Updated Chatbot

--- src/components/Home/MapPropertySection.tsx
+++ src/components/Home/MapPropertySection.tsx
@@ -6,18 +6,38 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
 import { useGetAllUsersPropertyQuery } from '@/redux/features/users/getAllUsersPropertyApi';
 
+
 const coordinateLookup: Record<string, [number, number]> = {
-  'Dhaka': [23.8103, 90.4125],
+    'Dhaka': [23.8103, 90.4125],
   'Mirpur': [23.8041, 90.3673],
   'Dhanmondi': [23.7461, 90.3742],
   'Uttara': [23.8759, 90.3795],
   'Gulshan': [23.7925, 90.4078],
   'Banani': [23.7937, 90.4066],
   'Mohammadpur': [23.7662, 90.3589],
+  'London': [51.5074, -0.1278],
   'Manchester': [53.4808, -2.2426],
   'Liverpool': [53.4084, -2.9916],
+  'Birmingham': [52.4862, -1.8904],
+  'Leeds': [53.7997, -1.5492],
+  'Sheffield': [53.3811, -1.4701],
+  'Glasgow': [55.8642, -4.2518],
+  'Edinburgh': [55.9533, -3.1883],
+  'Cardiff': [51.4816, -3.1791],
+  'Belfast': [54.5973, -5.9301],
   'Lancashire': [53.7632, -2.7044],
   'North Wales': [53.1362, -4.0954],
+  'South Wales': [51.6000, -3.9167],
+  'West Midlands': [52.4862, -1.8904],
+  'East Midlands': [52.8300, -1.3270],
+  'North East England': [54.9783, -1.6178],
+  'North West England': [53.483959, -2.244644],
+  'South East England': [51.3556, -0.5593],
+  'South West England': [51.4545, -2.5879],
+  'Yorkshire and the Humber': [53.9915, -1.5412],
+  'Scotland': [56.4907, -4.2026],
+  'Wales': [52.1307, -3.7837],
+  'Northern Ireland': [54.7877, -6.4923],
 };
 
 const MapPropertySection: React.FC = () => {
